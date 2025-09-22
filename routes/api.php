53725193e8de6d8acb5a5f3e6d5aba644bd897ce
Evgenia <glyxova04@gmail.com>
2025-09-22 12:39:08 +0500

    Установка Sanctum

--- routes/api.php
+++ routes/api.php
@@ -26,19 +26,18 @@ use App\Http\Controllers\AppointmentControllerApi;
 Route::get('/c', [AppointmentControllerApi::class, 'index']);
 Route::get('/appointments/{id}', [AppointmentControllerApi::class, 'show']);
 
-Route::middleware('auth:sanctum')->get('/appointments', [AppointmentControllerApi::class, 'index']);
+//Route::middleware('auth:sanctum')->get('/appointments', [AppointmentControllerApi::class, 'index']);
 
 use App\Http\Controllers\AuthController;
 
 Route::post('/login', [AuthController::class, 'login']);
 
-Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
-    return $request->user();
-});
-Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
+//Route::middleware('auth:sanctum')->get('/user', function (Request $request) { return $request->user();});
+
+//Route::middleware('auth:sanctum')->get('/logout', [AuthController::class, 'logout']);
 
 Route::group(['middleware' => 'auth:sanctum'], function () {
-    Route::post('/appointments', [AppointmentController::class, 'store']);
+    Route::get('/appointments', [AppointmentController::class, 'index']);
     Route::get('user',function(Request $request){
         return $request->user();
     });
